ae2fdc1a7a243fc2d7799fdca2492347b83d1503
khaivan <levankhai6729@gmail.com>
2022-06-21 15:09:46 +0700

    teyy

--- fairseq/models/__init__.py
+++ fairseq/models/__init__.py
@@ -98,9 +98,9 @@ def build_model(cfg: FairseqDataclass, task, from_checkpoint=False):
                 ARCH_CONFIG_REGISTRY[model_type](cfg)
 
     assert model is not None, (
-        f"Could not infer model type from {cfg}. "
-        "Available models: {}".format(MODEL_DATACLASS_REGISTRY.keys())
-        + f" Requested model type: {model_type}"
+            f"Could not infer model type from {cfg}. "
+            + "Available models: {}".format(MODEL_DATACLASS_REGISTRY.keys())
+            + f" Requested model type: {model_type}"
     )
 
     return model.build_model(cfg, task)
